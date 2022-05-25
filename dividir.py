6f977203c9298d2cb56901bf06d487bf46ed182f
Carlos Jiménez <caliche18a@gmail.com>
2022-05-25 17:04:15 -0500

    operación de dividir dos numeros que pide por consola

---
+++ dividir.py
@@ -0,0 +1,8 @@
+nombre = input("Digite su nombre: ")
+nu1 = int(input("Ingrese un número para dividir: "))
+nu2 = int(input("Ingrese el segundo número para dividir: "))
+res = nu1 / nu2
+
+print("Hola "+nombre+" el resultado es: "+str(res))
+
+print(type(res))
