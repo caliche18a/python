5ad3e62d0d14a44b3e39f6469819f81c6f0f7e9a
Carlos Jiménez <caliche18a@gmail.com>
2022-05-25 18:59:13 -0500

    promedio con while. Agregar break para romper. Sólo pedir 3 notas

--- promedio_while.py
+++ promedio_while.py
@@ -5,10 +5,9 @@ while nota != -1:
     total += nota
     contar += 1
 
-    nota = float(input("Ingrese la nota del estudiante (-1 para salir): "))
-
-    if contar == 9:
+    if contar == 3:
         break
+    nota = float(input("Ingrese la nota del estudiante (-1 para salir): "))
 
 promedio = total / contar
 print("El promedio de la nota es: "+str(promedio))
