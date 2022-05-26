3a275198b7dd8bc6cc9cc73594c5bf06a7431a6e
Carlos Jiménez <caliche18a@gmail.com>
2022-05-26 10:43:55 -0500

    Variables en Python

---
+++ variables.py
@@ -0,0 +1,9 @@
+nombre = "Carlos"
+apellido = "Jiménez"
+edad = 36
+ciudad = "Medellín"
+estatura = 1.62
+estudiante = True
+
+print("Mi nombre es "+nombre+" "+apellido+", mi edad es "+str(edad)+" años y vivo en "+ciudad+
+      ". Mi estatura es "+str(estatura)+" soy "+str(estudiante))
