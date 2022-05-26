b58ebd63e03f558ef757d2100dad80c1fdf365e8
Carlos Jiménez <caliche18a@gmail.com>
2022-05-26 15:07:03 -0500

    operadores lógicos y de comparación

---
+++ conversorMonedas.py
@@ -0,0 +1,8 @@
+pesos = float(input("¿Cuántos pesos colombianos tienes:? "))
+dolar = 3930.83
+
+dolares = pesos / dolar
+dolares = round(dolares, 2)
+
+print("Usted tiene hoy $ "+str(dolares)+" dólares")
+
