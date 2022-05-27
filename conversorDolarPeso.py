9ce398602c0ee0efc2047308301b80d08a9d2afa
Carlos Jiménez <caliche18a@gmail.com>
2022-05-26 21:08:20 -0500

    Convertir Dólar a Peso colombiano

---
+++ conversorDolarPeso.py
@@ -0,0 +1,5 @@
+dolar = float(input("Ingrese el valor en dólares: "))
+peso = 3925.50
+conversion = dolar * peso
+conversion = round(conversion,2)
+print("Usted tiene "+ str(conversion)+" pesos colombianos")
