cc9268094d7ad98dd0b22d1ad6f296fa3539b58e
Carlos Jiménez <caliche18a@gmail.com>
2022-05-26 21:00:16 -0500

    Convertir Peso a Dólar

--- conversorPesoDolar.py
+++ conversorPesoDolar.py
@@ -2,7 +2,7 @@ pesos = float(input("¿Cuántos pesos colombianos tienes:? "))
 dolar = 3930.83
 
 dolares = pesos / dolar
-dolares = round(dolares, 2)
+dolares = round(dolares, 1)
 
 print("Usted tiene hoy $ "+str(dolares)+" dólares.")
 
